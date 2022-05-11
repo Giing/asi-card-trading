56f86b0e331d1ef530da0a9ebbe87a9910c30446
bertrand <bertrand.pautet@cpe.fr>
2022-05-11 16:38:57 +0200

    init

---
+++ src/test/java/com/asi/cardtrading/CardTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asi.cardtrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
