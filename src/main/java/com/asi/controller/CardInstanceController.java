b73f67c49cecf788cbaee8fb679136d1960fa986
Giing <lazareth.baptiste@gmail.com>
2022-05-23 17:28:31 +0200

    récupération de toutes les cartes d'un utilisateur

--- src/main/java/com/asi/controller/CardInstanceController.java
+++ src/main/java/com/asi/controller/CardInstanceController.java
@@ -1,12 +1,29 @@
 package com.asi.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.asi.model.CardInstance;
 import com.asi.service.CardInstanceService;
 
 @RestController
 public class CardInstanceController {
+	
 	@Autowired
 	CardInstanceService cardInstanceService;
+	
+	@RequestMapping(method=RequestMethod.GET, value="/cards/users/{idUser}")
+	public List<CardInstance> getUserCards(@PathVariable int idUser) {
+		
+		System.out.println("Requête : Toutes les cartes de l'utilisateur");
+		List<CardInstance> cards = cardInstanceService.getCardsByUser(idUser);
+		
+		return cards;
+	}
 }
