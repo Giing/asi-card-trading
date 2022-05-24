68bdef31da75e6ea62659c997105571073c189f1
Giing <lazareth.baptiste@gmail.com>
2022-05-24 11:16:29 +0200

    Affichage cartes

--- src/main/resources/static/js/components/card.js
+++ src/main/resources/static/js/components/card.js
@@ -55,7 +55,7 @@ class FullCard extends HTMLBindableElement {
                             </div>
                             <div class="ui fluid image">
                                 <a class="ui left corner label">
-                                    DEADPOOL
+                                    ${this.card.nameCard}
                                 </a>
                                 <img id="cardImgId" class="ui centered image" src="${this.card.img_src}">
                             </div>
@@ -65,26 +65,26 @@ class FullCard extends HTMLBindableElement {
                         <div class="ui form tiny">
                             <div class="field">
                                 <label id="cardNameId"></label>
-                                <textarea id="cardDescriptionId" class="overflowHiden" readonly="" rows="2">Le convoi d'Ajax est attaqué par Deadpool. Il commence par massacrer les gardes à l'intérieur d'une voiture, avant de s'en prendre au reste du convoi. Après une longue escarmouche, où il est contraint de n'utiliser que les douze balles qu'il lui reste, Deadpool capture Ajax (dont le véritable nom est Francis, ce que Deadpool ne cesse de lui rappeler). Après l'intervention de Colossus et Negasonic venus empêcher Deadpool de causer plus de dégâts et le rallier à la cause des X-Men, Ajax parvient à s'échapper en retirant le sabre de son épaule. Il apprend par la même occasion la véritable identité de Deadpool : Wade Wilson.
+                                <textarea id="cardDescriptionId" class="overflowHiden" readonly="" rows="2">${this.card.descriptionCard}
                                 </textarea>
                             </div>
                         </div>
                     </div>
                     <div class="content">
-                        <i class="heart outline icon"></i><span id="cardHPId"> HP 10</span>
+                        <i class="heart outline icon"></i><span id="cardHPId"> HP ${this.card.hpCard}</span>
                         <div class="right floated ">
-                            <span id="cardEnergyId">Energy 10</span>
+                            <span id="cardEnergyId">Energy ${this.card.energyCard}'</span>
                             <i class="lightning icon"></i>
             
                         </div>
                     </div>
                     <div class="content">
                         <span class="right floated">
-                            <span id="cardAttackId"> Attack 20</span>
+                            <span id="cardAttackId"> Attack ${this.card.attackCard}</span>
                             <i class=" wizard icon"></i>
                         </span>
                         <i class="protect icon"></i>
-                        <span id="cardDefenceId">Defense 500</span>
+                        <span id="cardDefenceId">Defense ${this.card.defenceCard}</span>
                     </div>
                     <div class="field">
                         <div class="ui left icon input" id="sellButton">
